0d5c133b5e305a8453d1efe96173b03c80cd36e9
Diego Oliveira <diegooliveiras.ti@gmail.com>
2025-12-05 21:31:04 -0300

    fix: generator_api_routes

--- generator_api_routes.py
+++ generator_api_routes.py
@@ -1,16 +1,10 @@
 import json
 
-
-with open('coins.json',encoding='utf-8') as file:
-    coins = json.load(file)
-
-
 def generator_coins_route():
+    with open('coins.json',encoding='utf-8') as file:
+        coins = json.load(file)
     coins_api_route = {}
     for coin in coins:
         coins_api_route[coin['symbol']] = coin['id']
-
-    with open('coins_route.json', encoding='utf-8',mode='w') as file:
-        file.write(json.dumps(ensure_ascii=True,indent=4,obj=coins_api_route))
     return coins_api_route
 
